264bfb0385c04d933be751c67ce2fd62af426666
appdata <appdata@qq.com>
2024-11-25 15:29:01 +0800

    汉化

--- ui/src/components/molecules/DAGStatusOverview.tsx
+++ ui/src/components/molecules/DAGStatusOverview.tsx
@@ -4,12 +4,17 @@ import StatusChip from '../atoms/StatusChip';
 import { Stack } from '@mui/material';
 import LabeledItem from '../atoms/LabeledItem';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 type Props = {
   status?: Status;
   name: string;
   file?: string;
 };
+
+function formatTime(time: any) {
+  return moment(time).format('YYYY-MM-DD HH:mm:ss')
+}
 
 function DAGStatusOverview({ status, name, file = '' }: Props) {
   const url = `/dags/${name}/scheduler-log?&file=${encodeURI(file)}`;
@@ -22,8 +27,8 @@ function DAGStatusOverview({ status, name, file = '' }: Props) {
         <StatusChip status={status.Status}>{status.StatusText}</StatusChip>
       </LabeledItem>
       <LabeledItem label="请求ID">{status.RequestId}</LabeledItem>
-      <LabeledItem label="开始时间">{status.StartedAt}</LabeledItem>
-      <LabeledItem label="完成时间">{status.FinishedAt}</LabeledItem>
+      <LabeledItem label="开始时间">{formatTime(status.StartedAt)}</LabeledItem>
+      <LabeledItem label="完成时间">{formatTime(status.FinishedAt)}</LabeledItem>
       <LabeledItem label="参数">{status.Params}</LabeledItem>
       <LabeledItem label="计划任务日志">
         <Link to={url}>{status.Log}</Link>
